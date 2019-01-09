b9bf2de9dad1fe3f6907257973c53d339647e409
roshantak <roshantak44@gmail.com>
2019-01-09 20:03:09 +0530

    9jan

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var weburls = require('./models/weburls');
 var app = express();
-var url = "mongodb://roshan:roshantak44@ds147734.mlab.com:47734/nodemo";
+var url = "mongodb://localhost:27017/nodem";
 var port = 3000;
  
 // all environments
@@ -20,19 +20,69 @@ mongoose.set('useCreateIndex', true)
 mongoose.connect(url, { useNewUrlParser: true });
 
 
-app.get('/dbusiness', function(req, res){
+app.get('/weburls/:domainname', function(req, res, next){
+    var domainname = req.params.domainname;
     console.log('home');
-	weburls.find({domain:"business"}).then(eachOne => {
-    res.json(eachOne);
+    weburls.find({domain: domainname}, function(err, eachOne){
+        if (err){
+            next(err);
+            res.send("error is there");
+          }
+          else{
+    console.log(req);
+    res.json(eachOne.length);
     console.log(eachOne);
-	});
+             }
+            });
+});
+app.post('/weburls/report',function(req,res){
+    console.log("Boom");
+    const q1 = weburls.find({$or: [{$where: "this.phone.length>0"}, {$where: "this.mobile.length>0"}, {$where: "this.email.length>0"}]},{_id:1}).exec(function(err,item1){
+    console.log("boom1");
+    if(!err && item1)
+    {   
+    const q2= weburls.find({isCrawled : false, depth : 1},{_id:1}).exec(function(err,item2){   
+    console.log("boom2");
+    if(!err && item2)
+    {
+    const q3= weburls.find({isCrawled : true},{_id:1}).exec(function(err,item3){
+    console.log("boom3");
+    if(!err && item3)
+    {
+        var obj={};
+        obj.q1=item1.length;
+        obj.q2=item2.length;
+        obj.q3=item3.length;
+    res.json(obj);
+    console.log(obj);
+    }    
+    });
+    }                    
+    });    
+    }
+    else
+        {
+            console.log("Error: " + err);
+            res.json({message: "Error in mongo"});
+        }
+    });    
 });
 app.get('/dgt2', function(req, res){
     console.log('home');
-	weburls.find({depth: {'$gt':2}}).then(eachOne => {
-    res.json(eachOne);
+	weburls.find(({depth: {'$gt':2}}), function (err, eachOne) {
+    console.log(req);
+    res.json(eachOne.length, {hello: 'hlw'});
     console.log(eachOne);
-	});
+    });
+});
+app.get('/dgto2', function(req, res){
+    console.log('home');
+	weburls.find(({depth: {'$gt':2}}), function (err, eachOne) {
+    console.log(req);
+    res.json(eachOne.length);
+    console.log(eachOne);
+    res.end('I am done');
+    });
 });
 app.get('/errort', function(req, res){
     console.log('home');
@@ -43,7 +93,7 @@ app.get('/errort', function(req, res){
 });
 app.get('/uph', function(req, res){
     console.log('home');
-	weburls.find({ "url.protocol": "http:"}).then(eachOne => {
+	weburls.find({"url.protocol": "http:"}).then(eachOne => {
     res.json(eachOne);
     console.log(eachOne);
 	});
@@ -51,4 +101,4 @@ app.get('/uph', function(req, res){
 
 app.listen(port, function(){
     console.log('hii i am listing to '+port);
-})
+});
